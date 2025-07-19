40b3138b8dda062d977f3ccd18505948a1e8e0db
JuniorDLR <juniordelarocha15@hotmail.com>
2025-07-19 13:03:37 -0600

    Feat: Configuración de la base de datos Room
    Se crea la clase AppDatabase y se definen las entidades y DAOs.

---
+++ app/src/main/java/com/theburgerclub/burgercredit/data/local/database/AppDatabase.kt
@@ -0,0 +1,21 @@
+package com.theburgerclub.burgercredit.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.theburgerclub.burgercredit.data.local.dao.CustomerDao
+import com.theburgerclub.burgercredit.data.local.dao.DishDao
+import com.theburgerclub.burgercredit.data.local.dao.DebtDao
+import com.theburgerclub.burgercredit.data.local.entity.CustomerEntity
+import com.theburgerclub.burgercredit.data.local.entity.DishEntity
+import com.theburgerclub.burgercredit.data.local.entity.DebtEntity
+
+@Database(
+    entities = [CustomerEntity::class, DishEntity::class, DebtEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun customerDao(): CustomerDao
+    abstract fun dishDao(): DishDao
+    abstract fun debtDao(): DebtDao
+} 
